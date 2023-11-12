a42fc8ab508124bae89cb8a85b17b4569c9f9d0c
jaredkominsky <83463012+jaredkominsky@users.noreply.github.com>
2023-11-12 13:11:30 -0500

    change tf state (#11)
    * added on pr automation
    
    * syntax fix
    
    * changing tf plan to different yaml file
    
    * trying different tf directory
    
    * trying default directory
    
    * trying to see directory
    
    * trying new directory location
    
    * moving to one pipelines, trying out caching
    
    * syntax fix
    
    * update versions
    
    * added on push
    
    * changing tf state to cloud

--- tf/versions.tf
+++ tf/versions.tf
@@ -1,11 +1,11 @@
 terraform {
   required_version = ">= 1.6.0"
 
-  backend "s3" {
-    bucket         = "tf-statelock"
-    key            = "repo-creation.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "tf-state-table"
-    encrypt        = true
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "KominskyOrg"
+    workspaces {
+      tags = ["repo-creation"]
+    }
   }
 }

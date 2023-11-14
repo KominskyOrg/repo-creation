90b3687c24da492e1419d2afded3c4c13c91c79c
jaredkominsky <83463012+jaredkominsky@users.noreply.github.com>
2023-11-14 11:56:41 -0500

    changed repos to public (#13)
    * changed repos to public
    
    * adding aws creds as var blocks
    
    * trying to add vars to aws provider

--- tf/provider.tf
+++ tf/provider.tf
@@ -4,5 +4,7 @@ provider "github" {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,1 +1,3 @@
 variable "github_token_ssm_name" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
